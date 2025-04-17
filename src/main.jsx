7901ef0c34c34cf263d162a7c6247abe3f2dbcec
youssef <youssefmansi2@gmail.com>
2025-04-17 02:44:54 +0200

    y

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
 import { ThemeProvider } from "@material-tailwind/react";
 import { BrowserRouter } from 'react-router-dom';
+import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
@@ -13,5 +14,5 @@ createRoot(document.getElementById('root')).render(
       <App />
     </ThemeProvider>
     </BrowserRouter>
- ,
+ 
 )
